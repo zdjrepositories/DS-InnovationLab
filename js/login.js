5b5acb73ac780a904d5c2964cc49ab1700acb3a5
张冬军 <zdjyx126@126.com>
2020-02-26 15:15:52 +0800

    二期初步完成

--- js/login.js
+++ js/login.js
@@ -1,10 +1,22 @@
 function login() {
-    if ($('#mail').val() == ""  ) {
-        $(".login-input-mail").css("opacity", 1); //点击登录后显示loading，隐藏输入框
-    } else if($('#password').val() == ""){
-        $(".login-input-password").css("opacity", 1); //点击登录后显示loading，隐藏输入框
+    if ($('#email').val() == ""  ) {
+        $(".login-input-mail-error").text("请输入邮箱")
+        $(".login-input-mail-error").css("opacity", 1); //点击登录后显示loading，隐藏输入框
+    } else if($('#email').val().indexOf("@se.com")<0 && $('#email').val().indexOf("@non.se.com")<0){
+        $(".login-input-mail-error").text("邮箱格式错误")
+        $(".login-input-mail-error").css("opacity", 1); //点击登录后显示loading，隐藏输入框
+    }else if($('#password').val() == ""){
+        $(".login-input-password-error").text("请输入密码")
+        $(".login-input-password-error").css("opacity", 1); //点击登录后显示loading，隐藏输入框
+    }else if($('#code').val() == ""){
+        $(".login-input-code-error").text("请输入验证码")
+        $(".login-input-code-error").css("opacity", 1); //点击登录后显示loading，隐藏输入框
+    }else if($('#code').val()!= sum){
+        $(".login-input-code-error").text("验证码错误")
+        $(".login-input-code-error").css("opacity", 1); //点击登录后显示loading，隐藏输入框
     }
     else  {
+
         $.post("/innovationlab/login",{"email":$('#email').val(),"password":$('#password').val()},function (data) {
            if(data=="1"){
            var page= GetQueryString("page")+".html";
@@ -39,3 +51,52 @@ function pwdno(){
     $(".login-pwd").css("opacity", 1); //点击登录后显示loading，隐藏输入框
     $("#password").attr("type","password");
 }
+
+$(function () {
+    var sum;
+    $("#email").focus(function(){
+        $(".login-input-mail").css("opacity", 1);
+        $(".login-input-mail-error").css("opacity", 0);
+    });
+    $("#email").blur(function(){$(".login-input-mail").css("opacity", 0);});
+    $("#password").focus(function(){
+        $(".login-input-password").css("opacity", 1);
+        $(".login-input-password-error").css("opacity", 0);
+    });
+    $("#password").blur(function(){$(".login-input-password").css("opacity", 0);});
+    $("#code").focus(function(){
+        $(".login-input-code").css("opacity", 1);
+        $(".login-input-code-error").css("opacity", 0);
+    });
+    $("#code").blur(function(){$(".login-input-code").css("opacity", 0);});
+
+   code()
+
+});
+function code() {
+    var numa=Math.floor(Math.random() * 100);
+    var numb=Math.floor(Math.random() * 100);
+    var x=Math.floor(Math.random() *2);
+    var char;
+    if(x==0){
+        char='+';
+        sum=numa+numb;
+    }else {
+        char='-';
+        sum=numa+numb;
+    }
+    $(".login-code-text").text(numa+char+numb+"=?");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
